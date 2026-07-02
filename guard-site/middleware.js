80a5925fd5ba477a72576d5eab08e9f5b7c742e1
vizon1 <128507571+vizon1@users.noreply.github.com>
2026-07-02 14:51:41 -0300

    Update middleware.js

--- guard-site/middleware.js
+++ guard-site/middleware.js
@@ -1,25 +1,21 @@
 export const config = {
-    // 🚨 AQUI ESTÃO AS PÁGINAS PROTEGIDAS
     matcher: [
         '/admin',
         '/painel',
         '/gerenciamento-anuncios',
-        '/gerenciador-caixas'
+        '/gerenciador-caixas',
+        '/gerenciamento-nfe.html',
+        '/gerenciamento-envios.html'
     ],
 };
 
 export default function middleware(request) {
-    // Pega os cookies diretamente do cabeçalho
     const cookieHeader = request.headers.get('cookie') || '';
     
-    // Verifica se o cookie invisível de Admin está presente
     const hasAdminToken = cookieHeader.includes('guard_admin_token=');
 
     if (!hasAdminToken) {
-        // Se a pessoa NÃO TIVER o token, redireciona para a loja imediatamente
         const url = new URL('/store', request.url);
         return Response.redirect(url, 302);
     }
-
-    // Se for um Admin, deixa a página carregar
 }
